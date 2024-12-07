5ec1f38b0ca76d982d14bb4ca90852a300861ddb
DockerComposeFiles <rthem@gmx.de>
2024-12-07 20:26:08 +0100

    import table on ctrl

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/usb', [UsbController::class, 'insert']);
+Route::post('/usb', [UsbController::class, 'insert'])->name('usb');
